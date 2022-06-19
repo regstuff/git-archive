4d48a7a4928705bf709ea6dfd2aae9f215c19686
regstuff <btech11+github@gmail.com>
2022-06-19 20:53:01 +0530

    Get url from argument

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-url = 'https://isha.sadhguru.org/in/en/wisdom'
+import os, sys
+url = sys.argv[1]
 fname = 'html/' + url.replace('/', '_-_') + '.html'
 monolith = os.popen(f'chmod +x monolith && ./monolith -aevf {url} -o {fname}').read()
