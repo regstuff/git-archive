b6762b4475d9db181f74b336ce17d097e0c36218
regstuff <btech11+github@gmail.com>
2022-06-20 10:28:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-import os, sys
+import os, sys, json
 print('Arguments:',sys.argv)
+
 fullurl = sys.argv[1]
 url = fullurl.split('::')[0]
+
 commenter = sys.argv[2]
 print('Commenter:',commenter)
+
 owner = sys.argv[3]
 print('Owner:',owner)
-fname = 'html/' + url.replace('/', '_-_') + '.html'
-monolith_command = f'chmod +x monolith && ./monolith -aevf {url} -o {fname}'
+
+with open('config.json') as json_file: default_flags = json.load(json_file)
+print('Monolith Default Flags:',default_flags)
+
+if url[-5:] == '.html': fname = 'html/' + url.replace('/', '_-_') + '.html'
+else: fname = 'html/' + url.replace('/', '_-_')
+
+if '::' in fullurl and fullurl.split('::')[1] != '': monolith_command = f'chmod +x monolith && ./monolith {fullurl.split('::')[1]} {url} -o {fname}'
+elif '::' in fullurl: monolith_command = f'chmod +x monolith && ./monolith {url} -o {fname}'
+else: monolith_command = f'chmod +x monolith && ./monolith -ifave {url} -o {fname}'
+  
 print('Executing:',monolith_command)
-monolith = os.popen().read()
+monolith = os.popen(monolith_command).read()
