c04afea0f6b279ea809962a9172f229b437af3e2
regstuff <btech11+github@gmail.com>
2022-06-20 10:11:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os, sys
-url = sys.argv[1]
+fullurl = sys.argv[1]
+url = fullurl.split('::')[0]
+commenter = sys.argv[2]
+print('Commenter:',commenter)
+owner = sys.argv[3]
+print('Owner:',owner)
 fname = 'html/' + url.replace('/', '_-_') + '.html'
-monolith = os.popen(f'chmod +x monolith && ./monolith -aevf {url} -o {fname}').read()
+monolith_command = f'chmod +x monolith && ./monolith -aevf {url} -o {fname}'
+print('Executing:',monolith_command)
+monolith = os.popen().read()
